ed230bddaecb48f669936243e2ee504bded2bf7d
yasuyukiMaedaJ4J <y.maeda@j4j.co.jp>
2025-06-29 17:24:23 +0900

    コメントの追加

--- src/main/java/controller/MenuServlet.java
+++ src/main/java/controller/MenuServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 public class MenuServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		// 最初の画面です(メニュー)
 		RequestDispatcher dispatcher = request.getRequestDispatcher("menu.jsp");
 		dispatcher.forward(request, response);
 	}
